48e7d5f1272a91c2208238977aa39922773d3c98
lenarduzziadam <lenarduzziadam@gmail.com>
2024-10-01 10:51:44 -0500

    Sprite and confifg files

---
+++ config.py
@@ -0,0 +1,18 @@
+#Sizes
+SCREEN_WIDTH = 800;
+SCREEN_HEIGHT = 600;
+TILESIZE = 30;
+
+#Framerates
+FPS = 60;
+
+#Colors
+MPURPLE = (100, 20, 135);
+BLACK = (0, 0, 0);
+
+#Player 
+PLAYER_LAYER = 1;
+
+
+#Enviorment
+GROUND_LAYER = 1;
