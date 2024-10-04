bb4d294f0af95f1ea70f7f9a6086342cd4c48e3a
lenarduzziadam <lenarduzziadam@gmail.com>
2024-10-04 16:34:19 -0500

    added debug file

---
+++ debug.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+font = pygame.font.Font(None, 20)
+
+def debug(info, y = 10, x = 20):    
+    display_surface = pygame.display.get_surface()
+    debug_surf = font.render(str(info), True, 'White')
+    debug_rect = debug_surf.get_rect(topleft = (x, y))
+    pygame.draw.rect(display_surface, 'Black', debug_rect)
+    display_surface.bliy(debug_surf, debug_rect)
