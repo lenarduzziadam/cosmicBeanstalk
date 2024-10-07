1af3ba534c07f09278130b2f35198e273b4c93e7
lenarduzziadam <lenarduzziadam@gmail.com>
2024-10-07 13:57:53 -0500

    Added some new spells and fixed element call

--- weapon.py
+++ weapon.py
@@ -9,6 +9,7 @@ class Weapon:
         
         self.name = name
         self.weapon_type = weapon_type
+        self.element = element
         self.damage = damage
         self.value = value
         
@@ -19,26 +20,30 @@ uchigatana = Weapon("Uchigatana", "Sharp curved", "N/a", 29, 42)
 
 steel_longsword = Weapon("Steel Longsword", "Sharp", "N/a", 20, 30)
 
-drake_slayer = Weapon("Drakeslayer", "Blunt", "Dragon", 70, 500)
+drake_slayer = Weapon("Drakeslayer", "Blunt", "dragon", 70, 500)
 
 fishers_pike = Weapon("Fisher's Pike", "Pointy", "N/a", 18, 28)
 
-giants_hooks = Weapon("Hook of a Giant", "Pointy curved", "Arcane", 24, 35)
+giants_hooks = Weapon("Hook of a Giant", "Pointy curved", "arcane", 24, 35)
 
 fists = Weapon("Good Ol Left Hook", "Blunt", "N/a", 7, 0)
+
 
 #Ranged Weapons
 thorned_bow = Weapon("Thorned Bow", "Ranged magic", "Earth", 16, 25)
 
 hunters_bow = Weapon("Hunter's bow", "Ranged", "N/a", 12, 20)
 
-#spellcasting
-sumer_rage = Weapon("Rage of Sumeria", "AoE Spell", "Earth", 60, 150)
 
-gilgamesh_fall = Weapon("Fall of Gilgamesh", "Ranged Spell", "Earth", 75, 700)
+#spellcasting
+sumer_rage = Weapon("Rage of Sumeria", "AoE Spell", "earth", 60, 150)
+
+gilgamesh_fall = Weapon("Fall of Gilgamesh", "Ranged Spell", "earth", 75, 700)
 
 water_reflect = Weapon("Waters Reflection", "Melee Spell", "water", 40, 70)
 
 enflamed_kasenaru = Weapon("Enflamed Kasenaru", "Ranged Spell", "fire", 50, 130)
 
+stalking_vines = Weapon("Vines of the Great Stalk", "Ranged Spell", "arcane", 80, 1000)
 
+wolfs_moon = Weapon("Howlers Moon", "AoE Spell", "cosmic", 45, 650)
